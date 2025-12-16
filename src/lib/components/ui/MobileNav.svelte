6dc08f222cfa43ae6dd6c0a45573d6a9159c4492
mikeyboy52 <mikevanlommel2007@gmail.com>
2025-12-16 13:41:25 +0100

    Icons active gemaakt

--- src/lib/components/ui/MobileNav.svelte
+++ src/lib/components/ui/MobileNav.svelte
@@ -1,17 +1,19 @@
 <script lang="ts">
   import { goto } from "$app/navigation";
   import { page } from "$app/stores";
+	import Footer from "../layout/Footer.svelte";
 
   type NavItem = {
     label: string;
-    icon: string; // path to image file
+    iconActive: string; // path to image file
+    iconInactive: string; // path to image file
     href: string;
   };
 
   export let items: NavItem[] = [
-    { label: 'Home', icon: '/icons/HomeInactive.svg', href: '/' },
-    { label: 'Zoeken', icon: '/icons/Search.svg', href: '/search' },
-    { label: 'Profiel', icon: '/icons/Profile.svg', href: '/profile' }
+    { label: 'Home', iconActive: '/icons/DMHomeActive.svg', iconInactive: '/icons/DMHomeInactive.svg', href: '/' },
+    { label: 'Zoeken', iconActive: '/icons/DMSearchActive.svg', iconInactive: '/icons/DMSearchInactive.svg', href: '/search' },
+    { label: 'Profiel', iconActive: '/icons/DMProfileActive.svg', iconInactive: '/icons/DMProfileInactive.svg', href: '/profile' }
   ];
 
   let activeIndex: number = 0;
@@ -28,19 +30,19 @@
   }
 
 </script>
-
+<Footer />
 <div class="fixed bottom-0 left-0 right-0 bg-white border-t border-gray-200 shadow flex justify-around h-16 z-50" role="navigation" aria-label="Mobile navigation">
   {#each items as item, index}
     <button
       type="button"
       class="flex flex-col items-center justify-center flex-1 text-gray-500 cursor-pointer transition-colors duration-200 hover:text-blue-400
-             {index === activeIndex ? 'text-blue-500 border-t-2 border-blue-500' : ''}
+             <!-- {index === activeIndex ? 'text-blue-500 border-t-2 border-blue-500' : ''} -->
              {index < items.length - 1 ? 'border-r border-gray-300' : ''}"
       on:click={() => navigate(index, item.href)}
       aria-current={index === activeIndex ? 'page' : undefined}
       aria-label={item.label}
     >
-      <img src={item.icon} alt={item.label} class="w-6 h-6 object-contain" />
+      <img src={index === activeIndex ? item.iconActive : item.iconInactive} alt={item.label} class="w-6 h-6 object-contain" />
     </button>
   {/each}
 </div>
