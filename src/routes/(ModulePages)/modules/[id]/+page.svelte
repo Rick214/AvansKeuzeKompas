ccb509562ee3da6711ec2650eb88b93e22fa8afa
mikeyboy52 <mikevanlommel2007@gmail.com>
2025-12-19 12:15:43 +0100

    fixed star of favorite

--- src/routes/(ModulePages)/modules/[id]/+page.svelte
+++ src/routes/(ModulePages)/modules/[id]/+page.svelte
@@ -60,17 +60,7 @@
 				>
 					<span>{$translations.favorite_text}</span>
 
-					<svg viewBox="0 0 24 24" 
-						class="w-5 h-5 transition" 
-						fill={favorites.has(module.id) ? 'currentColor' : 'none'} 
-						stroke="currentColor" 
-						stroke-width="2" 
-					> 
-						<path stroke-linecap="round" 
-							stroke-linejoin="round" 
-							d="M11.049 2.927c.3-.921 1.603-.921 1.902 0l2.062 6.347a1 1 0 00.95.69h6.674c.969 0 1.371 1.24.588 1.81l-5.4 3.922a1 1 0 00-.364 1.118l2.062 6.347c.3.921-.755 1.688-1.54 1.118l-5.4-3.922a1 1 0 00-1.176 0l-5.4 3.922c-.784.57-1.838-.197-1.539-1.118l2.062-6.347a1 1 0 00-.364-1.118L.49 11.774c-.783-.57-.38-1.81.588-1.81h6.674a1 1 0 00.95-.69l2.062-6.347z" 
-						/> 
-					</svg>
+					<i class="fa-{favorites.has(module.id) ? 'solid' : 'regular'} fa-star"></i>
 				</button>
 			</div>
 
