8c377cedc11780ac9e6555c17a02f32781a4a620
Rick214 <rickvoermans07@gmail.com>
2026-01-14 10:39:15 +0100

    done
    changed from local to session storage

--- src/lib/stores/auth.ts
+++ src/lib/stores/auth.ts
@@ -1,7 +1,7 @@
 import { writable } from 'svelte/store';
 import type { User } from '$lib/types/user';
-import { createLocalStorage, persist } from '@macfja/svelte-persistent-store';
+import { createSessionStorage, persist } from '@macfja/svelte-persistent-store';
 
-export const user = persist(writable<User>(), createLocalStorage(), 'user');
+export const user = persist(writable<User>(), createSessionStorage(), 'user');
 export const token = writable<string>();
 export const isAuthenticated = writable(false);
