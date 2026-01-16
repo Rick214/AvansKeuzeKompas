0e74593a1ffc4103af7e3a164b4443e2353e9f07
Rick214 <rickvoermans07@gmail.com>
2026-01-16 09:52:43 +0100

    Update +layout.server.ts

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -2,7 +2,7 @@ import { redirect } from '@sveltejs/kit';
 import type { LayoutServerLoad } from './$types';
 
 export const load: LayoutServerLoad = async ({ locals, url }) => {
-	if (url.pathname.startsWith('/login') || url.pathname.startsWith('/auth')) {
+	if (url.pathname.startsWith('/login') || url.pathname.startsWith('/auth') || url.pathname === '/') {
 		return {};
 	}
 
