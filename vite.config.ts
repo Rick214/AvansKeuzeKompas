d43e3ae63124f9eda72d538fceb3b8abaed77cf9
Rick214 <rickvoermans07@gmail.com>
2026-01-16 11:14:58 +0100

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,12 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [tailwindcss(), sveltekit()],
-	server: {
-		port: process.env.PORT ? Number(process.env.PORT) : 5173,
-		host: true
-	},
-	preview: {
-		allowedHosts: ['avanskeuzekompas.onrender.com']
-	}
+	plugins: [tailwindcss(), sveltekit()]
+	// server: {
+	// 	port: process.env.PORT ? Number(process.env.PORT) : 5173,
+	// 	host: true
+	// },
+	// preview: {
+	// 	allowedHosts: ['avanskeuzekompas.onrender.com']
+	// }
 });
