7fe82df902ba45374fa1f025ffed4e7b4a6e1dde
Niels <157754367+Niels2804@users.noreply.github.com>
2026-01-17 11:49:01 +0100

    worked on faq

--- src/lib/components/ui/FAQ.svelte
+++ src/lib/components/ui/FAQ.svelte
@@ -10,7 +10,7 @@
 
 <section class="my-24">
 	<div class="max-w-3xl mx-auto px-4 sm:px-6 lg:px-8">
-		<h2 class="text-4xl font-bold text-center mb-12">{$translations.faq.title}</h2>
+		<h2 class="text-3xl font-bold text-center mb-12">{$translations.faq.title}</h2>
 		<div class="space-y-4">
 	        {#each Object.values($translations.faq.questions) as faq, i}
 				<div class="border border-(--color-border) rounded-xl overflow-hidden">
@@ -18,13 +18,13 @@
 						on:click={() => toggle(i)}
 						class="w-full flex justify-between items-center p-5 text-left font-medium bg-(--color-surface) hover:opacity-85 duration-200 transition cursor-pointer"
 					>
-						<span>{faq.question}</span>
+						<span class="font-medium text-medium">{faq.question}</span>
 						<span class="ml-2 text-xl transform transition-transform duration-300"
 							class:rotate-45={openIndex === i}>+</span>
 					</button>
 
 					<div
-						class="px-5 overflow-hidden transition-all duration-300 bg-(--color-surface-alt)"
+						class="px-5 overflow-hidden transition-all duration-300 text-sm bg-(--color-surface-alt)"
 						style="max-height: {openIndex === i ? '200px' : '0px'}"
 					>
 						{#if typeof faq.answer === 'string'}
