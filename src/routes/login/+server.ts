fc039e25615c4abfcaec961b4c5c6c303fe90f88
Niels <157754367+Niels2804@users.noreply.github.com>
2026-01-27 12:25:20 +0100

    Update +server.ts

--- src/routes/login/+server.ts
+++ src/routes/login/+server.ts
@@ -5,8 +5,6 @@ export const POST = async ({ request, cookies }: RequestEvent): Promise<Response
 	const { email, password } = await request.json();
 
 	const { token, ...user } = await login(email, password);
-
-	const API_KEY = '9f8b7c6d5e4a3b2c1d0f9e8d7c6b5a4f';
 	
 	// Token stays HttpOnly
 	cookies.set('auth', token, {
