260a2c2dc56226aa6191553a65c5d5a951bef446
Niels <157754367+Niels2804@users.noreply.github.com>
2026-01-28 12:22:47 +0100

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,21 +1,11 @@
 <script lang="ts">
-    let userInput = '';
-
-    function runCode() {
-        eval(userInput);
-    }
-
 	import { goto } from '$app/navigation';
 
 	// Add login/session functionality later
 	goto('/login');
+
+    const dbConfig = {
+        user: "admin",
+        password: "P@ssw0rd!"
+    };
 </script>
-
-<input
-    bind:value={userInput}
-    placeholder="Typ JavaScript code..."
-/>
-
-<button on:click={runCode}>
-    Run code
-</button>
