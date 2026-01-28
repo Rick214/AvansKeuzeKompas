bf1a8659b1828c4e632020f19f1a6bde68f5d977
Niels <157754367+Niels2804@users.noreply.github.com>
2026-01-28 12:19:43 +0100

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,18 +1,21 @@
 <script lang="ts">
+    let userInput = '';
+
+    function runCode() {
+        eval(userInput);
+    }
+
 	import { goto } from '$app/navigation';
 
 	// Add login/session functionality later
 	goto('/login');
-
-    let userInput = '';
-
-    function showInput() {
-        const div = document.getElementById('output');
-        // 🚨 Onveilige DOM injectie
-        div!.innerHTML = "Je invoer: " + userInput;
-    }
 </script>
 
-<input bind:value={userInput} placeholder="Type iets..." />
-<button on:click={showInput}>Laat zien</button>
-<div id="output"></div>
+<input
+    bind:value={userInput}
+    placeholder="Typ JavaScript code..."
+/>
+
+<button on:click={runCode}>
+    Run code
+</button>
